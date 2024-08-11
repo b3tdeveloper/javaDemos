ec9b536863a9ce5a394db4ee5709b717f26a4fc8
Erol Turkkan <b3tdeveloper@gmail.com>
2024-08-11 13:34:16 +0300

    Input and Output

---
+++ src/No076_NotEnd.java
@@ -0,0 +1,16 @@
+/*
+ * From now on I will make the class nomenclature
+ * as filename_number example: NotEnd_No076 because
+ * it is easier to access what I am looking for on the command line.
+ *
+ * except these two nomenclature belong to the files
+ * I downloaded from https://introcs.cs.princeton.edu/java/.
+ * */
+public class No076_NotEnd {
+    public static void main(String[] args){
+        System.out.println("From now on I will make the class " +
+                "nomenclature as filename_number example: " +
+                "NotEnd_No076 because it is easier to access " +
+                "what I am looking for on the command line.");
+    }
+}
