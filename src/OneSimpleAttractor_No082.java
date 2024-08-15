9319a16bada2a5b4f42368279082b5d0b660f865
Erol Turkkan <b3tdeveloper@gmail.com>
2024-08-15 21:28:41 +0300

    StdDraw

---
+++ src/OneSimpleAttractor_No082.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+ *  Compilation:  javac OneSimpleAttractor.java
+ *  Execution:    java OneSimpleAttractor
+ *  Dependencies: StdDraw.java
+ *
+ *  A particle is attracted to the current location of the mouse.
+ *  Incorporates drag.
+ *
+ *
+ *  % java OneSimpleAttractor
+ *
+ *
+ *  Credits:  Jeff Traer-Bernstein
+ *
+ ******************************************************************************/
+
+public class OneSimpleAttractor_No082 {
+    public static void main(String[] args){
+        double rx = 0.0, ry = 0.0;  //position
+        double vx = 0.0, vy = 0.0;  //velocity
+        double mass = 1.0;          //mass
+        double dt = 0.5;            //time quantum
+        double drag = 0.1;          //mess around with this a bit
+        double attractionStrength = 0.01;
+
+        // do the animation
+        StdDraw.enableDoubleBuffering();
+        while (true){
+
+            // compute
+
+        }
+
+    }
+}
